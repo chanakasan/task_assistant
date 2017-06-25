42839c2e2130e5cbe84a9342da039f81990e80e2
Chanaka Sandaruwan <chanakasan@gmail.com>
2017-06-25 22:41:56 +0530

    add firebase

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
+import * as firebase from 'firebase';
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBYtrRBgJo87ETYiOVs61vp02QeMUqHzXk",
+  authDomain: "task-assistant.firebaseapp.com",
+  databaseURL: "https://task-assistant.firebaseio.com",
+  projectId: "task-assistant",
+  storageBucket: "",
+  messagingSenderId: "747252980992"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<Root />, document.getElementById('root'))
